0b5654c9fa8a704d34beed01e3158411d5eb1c37
Jules G <juliegil@umich.edu>
2018-04-09 11:39:38 -0400

    new files, beginning web scraping

---
+++ scrapinghtml.py
@@ -0,0 +1,5 @@
+#just to request the html
+#store it, will sort through it all later
+
+daystoscrape = [313, 314, 315, 316, 317, 318, 322, 323, 324, 325, 331, 402]
+baseurl = "http://www.espn.com/mens-college-basketball/scoreboard/_/date/20180"
