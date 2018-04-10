1672aca797017f84ea90b933103dbca1dcc9b8a4
Jules G <juliegil@umich.edu>
2018-04-10 13:06:14 -0400

    working on a function to scrape entire UO site

--- testfile.py
+++ testfile.py
@@ -21,8 +21,26 @@ while start != usepage:
     nextpage_soup = BeautifulSoup(nextpage_text, 'html.parser')
     otheritems = nextpage_soup.find_all(itemprop = "name")
     start = start + 1
-    
+
 for each in games:
     print(each.text)
 
 #anchors = page_soup.find_all('a', class_ = "mobileScoreboardLink")
+
+### WOMENS ###
+
+def UOwebscrape(gender):
+    baseurl = "https://www.urbanoutfitters.com/"
+    if gender = "mens":
+        categories = ["graphic-tees", "clothing", "jackets-coats", "bottoms", "shoes", "accessories", "grooming"]
+        for eachtype in categories:
+            pageonerequesturl = baseurl + gender + "-new-" + eachtype
+            #make this request, store information
+            #also grab pagination count
+            #request additional pages and store
+    elif gender = "womens":
+        #swimwear order switches
+        #vintage has no womens
+        categories = ["dresses", "clothing", "jackets", "bottoms", "intimates", "swimwear", "vintage-clothing", "beauty", "accessories", "shoes"]
+    else:
+        print("gender choice not recongized: {}".format(gender))
