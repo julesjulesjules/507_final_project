0a82883f2295970c9930959128ab3ec1f150b02d
Jules G <juliegil@umich.edu>
2018-04-18 11:15:56 -0400

    building more with rebuilding UO stuff

--- main_program.py
+++ main_program.py
@@ -12,32 +12,37 @@ sys.stdout = codecs.getwriter('utf-8')(sys.stdout.buffer)
 # internal
 
 from testfile.py import *
+from match_up_jsons.py import *
 
 ################################################################################
 
 # if i want to rebuild the database completely:
 
-#need to re-scrape Urban Outfitters
+    #need to re-scrape Urban Outfitters
 
-# run UOpricescrape(gender) from testfile.py
-# can probably create a function for writing the files???
-mensUO = UOpricescrape("mens")
-json_muo = json.dumps(mensUO)
-f_muo = open("price_UOmensdictionary.json","w")
-f_muo.write(json_muo)
-f_muo.close()
+    # run UOpricescrape(gender) from testfile.py
+    # can probably create a function for writing the files???
+    mensUO = UOpricescrape("mens")
+    json_muo = json.dumps(mensUO)
+    f_muo = open("price_UOmensdictionary.json","w")
+    f_muo.write(json_muo)
+    f_muo.close()
 
-womensUO = UOpricescrape("womens")
-json_wuo = json.dumps(womensUO)
-f_wuo = open("price_UOwomensdictionary.json","w")
-f_wuo.write(json_wuo)
-f_wuo.close()
+    womensUO = UOpricescrape("womens")
+    json_wuo = json.dumps(womensUO)
+    f_wuo = open("price_UOwomensdictionary.json","w")
+    f_wuo.write(json_wuo)
+    f_wuo.close()
 
-# need to rescrape anthropologie:
+    # need to rescrape anthropologie:
 
 
 
 # if i want to rebuild the database from current files:
+    # just need to read in files:
+    uofiles = read_in_UOfiles()
+    womens = matchUOwomens(uofiles[0], uofiles[1])
+    mens = matchUOmens(uofiles[2], uofiles[3])
 
 
 
