a0df7417c0a0878fa220fa4ed0416f6e909631d1
Jules G <juliegil@umich.edu>
2018-04-18 10:43:39 -0400

    beginning to frame out main program

---
+++ main_program.py
@@ -0,0 +1,16 @@
+################################################################################
+# external
+
+import requests
+import json
+from bs4 import BeautifulSoup
+import codecs
+import sys
+sys.stdout = codecs.getwriter('utf-8')(sys.stdout.buffer)
+
+################################################################################
+# internal
+
+from testfile.py import *
+
+################################################################################
