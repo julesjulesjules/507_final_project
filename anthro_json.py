8e2edf5e302f32ab369ae78102d2b4ebc62c8d30
Jules G <juliegil@umich.edu>
2018-04-18 15:55:22 -0400

    changed code to functions

--- anthro_json.py
+++ anthro_json.py
@@ -6,31 +6,33 @@ import codecs
 import sys
 sys.stdout = codecs.getwriter('utf-8')(sys.stdout.buffer)
 
+def read_in_ANTfiles():
+    ant = open("anthropologie_scrape.json","r")
+    ant_read = ant.read()
+    anthro = json.loads(ant_read)
+    ant.close()
+    return(anthro)
 
-ant = open("anthropologie_scrape.json","r")
-ant_read = ant.read()
-anthro = json.loads(ant_read)
-ant.close()
+def match_anthros(anthro):
+    matched_dictionary = {}
+    for each in anthro:
+        print(each) # categories
+        print(type(anthro[each])) # 2 (one for items, one for prices) #list
+        len_len = len(anthro[each][0])
+        n = 0
+        cat_list = []
+        while n < len_len:
+            insert_tup = (anthro[each][0][n], anthro[each][1][n])
+            cat_list.append(insert_tup)
+            n = n + 1
+        matched_dictionary[each] = cat_list
+    return(matched_dictionary)
 
-print(type(anthro))
-matched_dictionary = {}
-for each in anthro:
-    print(each) # categories
-    print(type(anthro[each])) # 2 (one for items, one for prices) #list
-    len_len = len(anthro[each][0])
-    n = 0
-    cat_list = []
-    while n < len_len:
-        insert_tup = (anthro[each][0][n], anthro[each][1][n])
-        cat_list.append(insert_tup)
-        n = n + 1
-    matched_dictionary[each] = cat_list
-
-for each in matched_dictionary:
+#for each in matched_dictionary:
     #print(each)
     #print(len(matched_dictionary[each]))
 
-    
+
 
 
 
