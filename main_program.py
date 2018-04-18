8d7a7b45e5ff6979b924fcad3c6031080ed78ef7
Jules G <juliegil@umich.edu>
2018-04-18 10:50:07 -0400

    added in UO price scrape information

--- main_program.py
+++ main_program.py
@@ -17,6 +17,18 @@ from testfile.py import *
 
 # if i want to rescrape urban outfitters then:
     # run UOpricescrape(gender) from testfile.py
+    # can probably create a function for writing the files???
+    mensUO = UOpricescrape("mens")
+    json = json.dumps(mensUO)
+    f = open("price_UOmensdictionary.json","w")
+    f.write(json)
+    f.close()
+
+    womensUO = UOpricescrape("womens")
+    json = json.dumps(womensUO)
+    f = open("price_UOwomensdictionary.json","w")
+    f.write(json)
+    f.close()
 
 # if i want to rescrape anthropologie:
 
