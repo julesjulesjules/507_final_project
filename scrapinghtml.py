db2eb70fd6d69712a6fc0ccb785689be5bfd1c23
Jules G <juliegil@umich.edu>
2018-04-18 10:41:04 -0400

    ignore -- is related to first idea re: espn

--- scrapinghtml.py
+++ scrapinghtml.py
@@ -1,6 +1,9 @@
 import requests
 import json
 from bs4 import BeautifulSoup
+import codecs
+import sys
+sys.stdout = codecs.getwriter('utf-8')(sys.stdout.buffer)
 
 #just to request the html
 #store it, will sort through it all later
@@ -12,16 +15,40 @@ baseurl = "http://www.espn.com/mens-college-basketball/scoreboard/_/date/20180"
 
 def grab_game_units(page_date):
     #baseurl for start page (from calendar)
-    baseurl = "http://www.espn.com/mens-college-basketball/scoreboard/_/date/20180"
+    baseurl = "http://www.espn.com/mens-college-basketball/scoreboard/_/group/100/date/20180"
     full_url = baseurl + str(page_date)
     page_text = requests.get(full_url).text
-    #page_soup = BeautifulSoup(page_text, 'html.parser')
+    page_soup = BeautifulSoup(page_text, 'lxml')
+    anchors = page_soup.find_all('a', class_ = "mobileScoreboardLink")
+    print(len(anchors))
     return page_text
 
-#writeoutfirsthtml = {}
-#for each_number in daystoscrape:
-#    writeoutfirsthtml[each_number] = grab_game_units(each_number)
+writeoutfirsthtml = {}
+for each_number in daystoscrape:
+    writeoutfirsthtml[each_number] = grab_game_units(each_number)
+
 
 ## write out to text file:
 #with open("roundone_scrape.txt", "w") as ftowrite:
 #    ftowrite.write(json.dumps(writeoutfirsthtml))
+
+#with open("roundone_scrape.txt", "r") as f:
+#    r = f.read()
+#    n = json.loads(r)
+
+#linklist = []
+#print(type(n))
+#for each in n:
+#    soup = BeautifulSoup(n[each], 'html.parser')
+#    emailitems = soup.find_all(class_ = "mobileScoreboardLink")
+
+#    for each in emailitems:
+#        emaillink = each.find_all(a['href'])
+#        linklist.append(emaillink)
+#print(linklist)
+
+#page_soup = BeautifulSoup(page_text, 'html.parser')
+#emailitems = page_soup.find_all('a', class_ = "mobileScoreboardLink")
+#nextlinklist = []
+#for each in emailitems:
+#    nextlinklist.append(emailitems.find('a')['href'])
