26cda3eaf83d49a4aadcadc6886a5e4700a164ce
Jules G <juliegil@umich.edu>
2018-04-18 17:06:19 -0400

    updated item search term

--- working_clothes_functions.py
+++ working_clothes_functions.py
@@ -17,7 +17,7 @@ def UOwebscrape(gender):
             #make this request, store information
             page_text = requests.get(pageonerequesturl).text
             page_soup = BeautifulSoup(page_text, 'html.parser')
-            page_items = page_soup.find_all(itemprop = "name")
+            page_items = page_soup.find_all(class_ = "c-product-tile__h3 c-product-tile__h3--regular")
 
             #also need to find prices
             page_one_item_list = []
@@ -38,7 +38,7 @@ def UOwebscrape(gender):
                 pageurl = pageonerequesturl + "?page=" + str(start)
                 nextpage_text = requests.get(pageurl).text
                 nextpage_soup = BeautifulSoup(nextpage_text, 'html.parser')
-                otheritems = nextpage_soup.find_all(itemprop = "name")
+                otheritems = nextpage_soup.find_all(class_ = "c-product-tile__h3 c-product-tile__h3--regular")
                 n = 0
                 for each in otheritems:
                     if n < 18:
@@ -69,7 +69,7 @@ def UOwebscrape(gender):
             #make this request, store information
             page_text = requests.get(pageonerequesturl).text
             page_soup = BeautifulSoup(page_text, 'html.parser')
-            page_items = page_soup.find_all(itemprop = "name")
+            page_items = page_soup.find_all(class_ = "c-product-tile__h3 c-product-tile__h3--regular")
             #also need to find prices
             page_one_item_list = []
             n = 0
@@ -90,7 +90,7 @@ def UOwebscrape(gender):
                     pageurl = pageonerequesturl + "?page=" + str(start)
                     nextpage_text = requests.get(pageurl).text
                     nextpage_soup = BeautifulSoup(nextpage_text, 'html.parser')
-                    otheritems = nextpage_soup.find_all(itemprop = "name")
+                    otheritems = nextpage_soup.find_all(class_ = "c-product-tile__h3 c-product-tile__h3--regular")
                     n = 0
                     for each in otheritems:
                         if n < 21:
@@ -106,6 +106,21 @@ def UOwebscrape(gender):
         return(returndictionary)
     else:
         print("gender choice not recongized: {}".format(gender))
+
+
+mensUOitems = UOwebscrape("mens")
+
+json_muoi = json.dumps(mensUOitems)
+f_muoi = open("UOmensdictionary.json","w")
+f_muoi.write(json_muoi)
+f_muoi.close()
+
+womensUOitems = UOwebscrape("womens")
+
+json_wuoi = json.dumps(womensUOitems)
+f_wuoi = open("UOwomensdictionary.json","w")
+f_wuoi.write(json_wuoi)
+f_wuoi.close()
 
 #mensUO = UOwebscrape("mens")
 
