536e858767be037a1ca623c06a4da025d40b2572
Jules G <juliegil@umich.edu>
2018-04-18 10:42:52 -0400

    commented out testing code, includes only the function UOpricescrape(gender)

--- testfile.py
+++ testfile.py
@@ -5,34 +5,6 @@ import codecs
 import sys
 sys.stdout = codecs.getwriter('utf-8')(sys.stdout.buffer)
 
-#baseurl = "https://www.urbanoutfitters.com/womens-new-arrivals"
-#page_text = requests.get(baseurl).text
-#page_soup = BeautifulSoup(page_text, 'html.parser')
-#items = page_soup.find_all(itemprop = "name")
-#pagelimit = page_soup.find(class_ = "o-pagination__li o-pagination__number--next")
-#print(pagelimit.text)
-# use page limit to determine how many pages to scrape
-#usepage = int(pagelimit.text)
-#start = 2
-#while start != usepage:
-#    pageurl = "https://www.urbanoutfitters.com/womens-new-arrivals?page="
-#    fullurl = pageurl + str(start)
-#    nextpage_text = requests.get(fullurl).text
-#    nextpage_soup = BeautifulSoup(nextpage_text, 'html.parser')
-#    otheritems = nextpage_soup.find_all(itemprop = "name")
-#    start = start + 1
-
-#for each in games:
-#    print(each.text)
-
-#anchors = page_soup.find_all('a', class_ = "mobileScoreboardLink")
-
-### WOMENS ###
-#page_text = requests.get("https://www.urbanoutfitters.com/mens-new-clothing").text
-#page_soup = BeautifulSoup(page_text, 'html.parser')
-#page_prices = page_soup.find_all(class_ = "c-product-meta__current-price")
-#for each in page_prices:
-#    print(each.text.strip()[1:])
 
 def UOpricescrape(gender):
     baseurl = "https://www.urbanoutfitters.com/"
@@ -123,9 +95,43 @@ def UOpricescrape(gender):
 #f.write(json)
 #f.close()
 
-womensUO = UOpricescrape("womens")
+#womensUO = UOpricescrape("womens")
 
-json = json.dumps(womensUO)
-f = open("price_UOwomensdictionary.json","w")
-f.write(json)
-f.close()
+#json = json.dumps(womensUO)
+#f = open("price_UOwomensdictionary.json","w")
+#f.write(json)
+#f.close()
+
+
+################################################################################
+## Scratch work
+################################################################################
+
+#baseurl = "https://www.urbanoutfitters.com/womens-new-arrivals"
+#page_text = requests.get(baseurl).text
+#page_soup = BeautifulSoup(page_text, 'html.parser')
+#items = page_soup.find_all(itemprop = "name")
+#pagelimit = page_soup.find(class_ = "o-pagination__li o-pagination__number--next")
+#print(pagelimit.text)
+# use page limit to determine how many pages to scrape
+#usepage = int(pagelimit.text)
+#start = 2
+#while start != usepage:
+#    pageurl = "https://www.urbanoutfitters.com/womens-new-arrivals?page="
+#    fullurl = pageurl + str(start)
+#    nextpage_text = requests.get(fullurl).text
+#    nextpage_soup = BeautifulSoup(nextpage_text, 'html.parser')
+#    otheritems = nextpage_soup.find_all(itemprop = "name")
+#    start = start + 1
+
+#for each in games:
+#    print(each.text)
+
+#anchors = page_soup.find_all('a', class_ = "mobileScoreboardLink")
+
+### WOMENS ###
+#page_text = requests.get("https://www.urbanoutfitters.com/mens-new-clothing").text
+#page_soup = BeautifulSoup(page_text, 'html.parser')
+#page_prices = page_soup.find_all(class_ = "c-product-meta__current-price")
+#for each in page_prices:
+#    print(each.text.strip()[1:])
