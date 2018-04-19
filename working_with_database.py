133d3394147bbc40688a839836d1551d6fba3bef
Jules G <juliegil@umich.edu>
2018-04-19 12:55:11 -0400

    finding items by price works

--- working_with_database.py
+++ working_with_database.py
@@ -42,13 +42,37 @@ def find_a_pattern():
 
 def find_high_low_price():
     ### need a list of items to look for?
+    il_statement = '''select Type from Category'''
+    cur.execute(il_statement)
+    itemlist = []
+    for each in cur:
+        itemlist.append(each[0])
+    ## print out this so user knows choices
+    print("*******************************************************************")
+    start = 0
+    a = 0
+    b = 6
+    c = 12
+    d = 18
+    #24 categories
+    while start < 6:
+        print("{:<19}  {:<19}  {:<19}  {:<19}".format(itemlist[a], itemlist[b], itemlist[c], itemlist[d]))
+        start = start + 1
+        a = a + 1
+        b = b + 1
+        c = c + 1
+        d = d + 1
+    print("*******************************************************************")
+    print("\n")
     cat_choice = input("What type of items are you looking for? ")
     gend_choice = input("Choose mens/womens/both: ")
     how_many = input("How many items would you like to look at? #/all ")
     top_bottom = input("Are you looking for most/least expensive? ")
 
-    cat_statement = '''select CategoryId FROM Category WHERE Type="{}"'''.format(cat_choice)
+    cat_statement = '''select Id FROM Category WHERE Type="{}"'''.format(cat_choice)
     ### grab the number, use it below
+    cur.execute(cat_statement)
+    cat_num = cur.fetchone()[0]
     ## save in cat_num
 
     statement = '''select Brand.Name, ItemName, ListPrice '''
@@ -62,17 +86,20 @@ def find_high_low_price():
         statement += '''AND Items.CategoryId={} '''.format(cat_num)
     if gend_choice == "both":
         statement += '''WHERE Items.CategoryId={} '''.format(cat_num)
+        
+    if top_bottom == "most":
+        statement += '''ORDER BY ListPrice DESC '''
+    if top_bottom == "least":
+        statement += '''ORDER BY ListPrice ASC '''
 
     if how_many != "all":
-        statement += '''LIMIT {}'''.format(how_many)
-
-    if top_bottom == "most":
-        statement += '''DESC '''
-    if top_bottom == "least":
-        statement += '''ASC '''
+        statement += '''LIMIT {} '''.format(how_many)
 
 
 
+    return(statement)
 
+
+print(find_high_low_price())
 
 conn.close()
