585f195aa52303151873027e7a1a5192c35c4e16
Jules G <juliegil@umich.edu>
2018-04-19 10:25:49 -0400

    test file for messing with database

---
+++ working_with_database.py
@@ -0,0 +1,9 @@
+#### working with database
+
+#import requests
+#import json
+#from bs4 import BeautifulSoup
+import codecs
+import sys
+import sqlite3
+sys.stdout = codecs.getwriter('utf-8')(sys.stdout.buffer)
