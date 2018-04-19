12db457d69b46301cd89c7c7c4bcf277bda7e151
Jules G <juliegil@umich.edu>
2018-04-19 10:30:55 -0400

    working on find a pattern - beginning choices

--- working_with_database.py
+++ working_with_database.py
@@ -7,3 +7,17 @@ import codecs
 import sys
 import sqlite3
 sys.stdout = codecs.getwriter('utf-8')(sys.stdout.buffer)
+
+try:
+    conn = sqlite3.connect("storeitem.db")
+    cur = conn.cursor()
+except Error as e:
+    print(e)
+    return("Failed to connect to database.")
+
+### find a pattern:
+def find_a_pattern():
+    pat_choice = input("What pattern are you looking for? stripe/dot/floral/plaid ")
+    gend_choice = input("Choose mens/womens/both: ")
+    how_many = input("How many items would you like to look at? #/all ")
+    
