a4f39e8a20b79ee522b65eab70358df4c8a3bf36
Jules G <juliegil@umich.edu>
2018-04-19 10:13:17 -0400

    simplified structure, added error while loop

--- main_program.py
+++ main_program.py
@@ -16,91 +16,34 @@ from match_up_jsons import *
 from anthroscrape import *
 from anthro_json import *
 from working_clothes_functions import *
+from second_level import *
 
 ################################################################################
 
-user_action = input("What would you like to do?")
-
-if user_action == "rebuild":
-# if i want to rebuild the database completely:
-
-    #need to re-scrape Urban Outfitters
-
-    # run UOpricescrape(gender) from testfile.py
-    # can probably create a function for writing the files???
-    mensUO = UOpricescrape("mens")
-    json_muo = json.dumps(mensUO)
-    f_muo = open("price_UOmensdictionary.json","w")
-    f_muo.write(json_muo)
-    f_muo.close()
-
-    womensUO = UOpricescrape("womens")
-    json_wuo = json.dumps(womensUO)
-    f_wuo = open("price_UOwomensdictionary.json","w")
-    f_wuo.write(json_wuo)
-    f_wuo.close()
-
-    mensUOitems = UOwebscrape("mens")
-
-    json_muoi = json.dumps(mensUOitems)
-    f_muoi = open("UOmensdictionary.json","w")
-    f_muoi.write(json_muoi)
-    f_muoi.close()
-
-    womensUOitems = UOwebscrape("womens")
-
-    json_wuoi = json.dumps(womensUOitems)
-    f_wuoi = open("UOwomensdictionary.json","w")
-    f_wuoi.write(json_wuoi)
-    f_wuoi.close()
-
-
-    # need to rescrape anthropologie:
-    ascrape = anthroScrape()
-    json_ant = json.dumps(ascrape)
-    f_ant = open("anthropologie_scrape.json","w")
-    f_ant.write(json_ant)
-    f_ant.close()
-
-### then continue
-elif user_action == "current":
-# if i want to rebuild the database from current files:
-    # just need to read in files:
-    uofiles = read_in_UOfiles()
-    womens = matchUOwomens(uofiles[0], uofiles[1])
-    mens = matchUOmens(uofiles[2], uofiles[3])
-
-    antfile = read_in_ANTfiles()
-    anthro_w = match_anthros(antfile)
-
-    urbanoutfitters = {"women": womens, "men": mens}
-    #set up db
-    make_db('storeitem.db')
-    #load in all accessory tables
-
-    gend = ["men", "women"]
-    insert_gend(gend)
-
-    bran = ["Anthropologie", "UrbanOutfitters"]
-    insert_brand(bran)
-
-    cats = []
-    for each in urbanoutfitters: #men women
-        for every in urbanoutfitters[each]:
-            if every not in cats:
-                cats.append(every)
-    for each in anthro_w:
-        if each not in cats:
-            cats.append(each)
-    insert_cates(cats)
-
-    insert_items(urbanoutfitters)
-    insert_items_ant(anthro_w)
-
-else:
-    print("unrecognized command: {}".format(user_action))
+user_action = input("What would you like to do (rebuild/current/none)? ")
+check = 0
+while check != 1:
+    if user_action == "rebuild":
+    # if i want to rebuild & rescrape
+        rescrape_and_rebuild()
+        check = 1
+    ### then continue
+    elif user_action == "current":
+    # if i want to rebuild the database from current files:
+        just_rebuild()
+        check = 1
+    elif user_action = "none":
+        print("Excellent, moving on then.")
+        check = 1
+    else:
+        print("unrecognized command: {}".format(user_action))
+        user_action = input("What would you like to do (rebuild/current/none)? ")
 
 ## then just use database
+print("***********************************************************************")
+print("                          Clothing Search                              ")
+print("***********************************************************************")
+print("\n")
 
 #elif user_action == "database":
     # if i want to use the current database
