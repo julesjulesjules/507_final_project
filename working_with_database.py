832fcd039389d14863be5aa636f05b406ed5f77d
Jules G <juliegil@umich.edu>
2018-04-19 10:52:49 -0400

    finding a pattern works

--- working_with_database.py
+++ working_with_database.py
@@ -13,11 +13,30 @@ try:
     cur = conn.cursor()
 except Error as e:
     print(e)
-    return("Failed to connect to database.")
+    print("Failed to connect to database.")
 
 ### find a pattern:
 def find_a_pattern():
     pat_choice = input("What pattern are you looking for? stripe/dot/floral/plaid ")
     gend_choice = input("Choose mens/womens/both: ")
     how_many = input("How many items would you like to look at? #/all ")
-    
+
+    statement = '''select Brand.Name, ItemName, ListPrice '''
+    statement += '''FROM Items JOIN Brand ON Brand.Id=Items.BrandId '''
+    ### gender
+    if gend_choice == "mens":
+        statement += '''WHERE Items.GenderId=1 '''
+        statement += '''AND ItemName LIKE '%{}%' '''.format(pat_choice)
+    if gend_choice == "womens":
+        statement += '''WHERE Items.GenderId=2 '''
+        statement += '''AND ItemName LIKE '%{}%' '''.format(pat_choice)
+    if gend_choice == "both":
+        statement += '''WHERE ItemName LIKE '%{}%' '''.format(pat_choice)
+
+    if how_many != "all":
+        statement += '''LIMIT {}'''.format(how_many)
+
+    return(statement)
+
+print(find_a_pattern())
+conn.close()
