81886c92c08ea2e0387b9de4b8528836b20d2f41
Jules G <juliegil@umich.edu>
2018-04-19 13:37:43 -0400

    added in database search items

--- main_program.py
+++ main_program.py
@@ -17,6 +17,7 @@ from anthroscrape import *
 from anthro_json import *
 from working_clothes_functions import *
 from second_level import *
+from working_with_database import *
 
 ################################################################################
 
@@ -50,8 +51,22 @@ user_instruct_or_no = input(">>>  ")
 if user_instruct_or_no == "help":
     pass
     ### print instructions
+    #print("***********************************************************************")
 elif user_instruct_or_no == "next":
     pass
 else:
     print("unrecognized command: {}".format(user_action))
     #### probably should also make this a while loop
+
+### search
+user_search_action = 0
+while user_search_action != "exit":
+    user_search_action = input("What search method would you like, pattern/price: ")
+    if user_search_action == "pattern":
+        find_a_pattern()
+    elif user_search_action == "price":
+        find_high_low_price()
+    elif user_search_action == "exit":
+        print("Goodbye!")
+    else:
+        print("unrecognized command: {}".format(user_search_action))
